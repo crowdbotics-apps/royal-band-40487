32a13d4c644c46e369bcfe4a231c772861b64b14
Crowdbotics <team@crowdbotics.com>
2023-05-05 22:23:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/royal_band_40487/MainActivity.java
@@ -0,0 +1,15 @@
+package com.royal_band_40487;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "royal_band_40487";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-band-40487",
+  url: "https://royal-band-40487.botics.co",
+  api: "https://royal-band-40487.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
